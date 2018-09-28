cb14823cd7a3f5c67601dbd22336ed1593e45610
liyuhai001 <liyuhai001@ke.com>
2018-09-28 10:33:00 +0800

    添加 aviator 引用jar 包，修改测试类路径

---
+++ src/test/java/com/packoi/app/ApplicationTests.java
@@ -0,0 +1,26 @@
+package com.packoi.app;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+/**
+ * @Description: 测试类入口
+ * @author yuhai.li  
+ * @date 2018年1月19日 下午2:36:10
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class ApplicationTests {
+	
+	@Test
+	public void contextLoads() {
+		
+	}
+
+}
